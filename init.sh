206fac42d02f9c0c4702df0866ab6e38c973bf5e
Roman Bovda <rbovda@spscommerce.com>
2020-05-19 18:18:00 +0300

    Update

--- init.sh
+++ init.sh
@@ -22,7 +22,7 @@ kubeadm version
 
 # This part is for Master node
 
-kubeadm init --pod-network-cidr=10.10.10.0/24
+kubeadm init --pod-network-cidr=10.10.0.0/16
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
