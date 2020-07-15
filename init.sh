6834159a9313dd6e752abca1ab325757b0ce8fc9
Roman Bovda <rbovda@spscommerce.com>
2020-07-15 13:21:51 +0300

    Add grafana/prometheus

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
 # Turn off swap
+sudo su
 swapoff -a
 cat <<EOF >> sudo /etc/fstab
 none        /var/run        tmpfs   size=1M,noatime         00
@@ -8,28 +10,38 @@ dphys-swapfile swapoff
 dphys-swapfile uninstall
 update-rc.d dphys-swapfile remove
 
-cat <<EOF > /boot/firmware/nobtcmd.txt
-cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1
+cat <<EOF > /boot/firmware/cmdline.txt
+cgroup_enable=memory cgroup_memory=1
 EOF
 
 # Install docker and kubernetes
 apt install docker.io -y
 systemctl enable docker
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee --append /etc/apt/sources.list
-apt update
-apt install kubeadm -y
 hostnamectl set-hostname slave-node
 free -m
 docker --version
-kubeadm version
+snap install microk8s --classic --channel=latest/stable
+snap info microk8s
+microk8s enable dashboard dns ingress metallb metrics-server rbac registry storage
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.6/components.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
+kubectl apply -f pihole/pihole.deploy.yml
+kubectl apply -f pihole/rbac.yml
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo update
+helm install prometheus stable/prometheus --namespace monitoring
+helm install stable/grafana -f monitoring/grafana/values.yml --namespace monitoring
 
-# This part is for Master node
+# Optional relaunch deployment with capable images
+# https://stackoverflow.com/questions/42885538/raspberry-pi-docker-error-standard-init-linux-go178-exec-user-process-caused
+kubectl delete deployment grafana -n monitoring
+kubectl delete deployment prometheus-kube-state-metrics -n monitoring
+kubectl apply -f monitoring/grafana/grafana.yml
+kubectl apply -f monitoring/prometheus.yml
+#=====-----
 
-kubeadm init --pod-network-cidr=10.10.0.0/16
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubectl get pods --all-namespaces
-kubectl get nodes
+kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+# port forwarding
+export POD_NAME=$(kubectl get pods --namespace monitoring -l "app=grafana,release=grafana" -o jsonpath="{.items[0].metadata.name}")
+kubectl --namespace monitoring port-forward $POD_NAME 3000
